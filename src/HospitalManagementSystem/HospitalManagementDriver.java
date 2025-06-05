27225a929deadfeabfe543cc23b6d3b952abf8d7
kamogotjhb024 <kamogotjhb024@student.wethinkcode.co.za>
2025-06-05 23:04:07 +0200

    creating Driver class

---
+++ src/HospitalManagementSystem/HospitalManagementDriver.java
@@ -0,0 +1,9 @@
+package HospitalManagementSystem;
+
+public class HospitalManagementDriver {
+
+    // class fields
+    private static final String url = "jdbc:mysql://localhost:3306/hospital_manager";
+    private static final String username = "root";
+    private static final String password = "Admin123";
+}
